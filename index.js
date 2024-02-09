56ec4c6d3ca9aa1f7199e8edecd03b2e287d18c0
yaniv <120222659+y4aniv@users.noreply.github.com>
2024-02-09 16:21:48 +0000

    server: add static file routes for public assets

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const dotenv = require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use('/public', express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/video', express.static(__dirname + 'public/video'));
+
 app.listen(port, () => {
     console.log(`Le serveur est lancé sur le port ${port}`);
 });
