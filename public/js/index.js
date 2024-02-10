27bb833e522c4928ee1bd948341068cd733cbd13
yaniv <120222659+y4aniv@users.noreply.github.com>
2024-02-10 18:15:48 +0000

    scripts: update scrollTrigger start percentage in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,10 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.to(".keywords__text span", {
     scrollTrigger: {
         trigger: ".keywords__text",
-        start: "10% 60%",
+        start: "10% 40%",
         end: "bottom 60%",
         scrub: 1,
+        markers: true,
     },
     opacity: 0.1,
     stagger: 0.1,
