9f4125db5a0e3b16f7c71fa25ddb93fdb0378c37
yaniv <120222659+y4aniv@users.noreply.github.com>
2024-02-18 23:27:48 +0000

    server: implement a POST endpoint for creating cocktails

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const editJsonFile = require('edit-json-file');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
@@ -12,6 +17,8 @@ app.use('/video', express.static(__dirname + 'public/video'));
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+var cocktailDB = editJsonFile('data/cocktails.json');
 
 app.get('/', (req, res) => {
     res.render('pages/index', {
@@ -57,6 +64,25 @@ app.get('/dining', (req, res) => {
         },
         le12: require('./data/le12.json'),
     });
+})
+
+app.post('/api/cocktail', (req, res) => {
+    const ingredients = req.body.ingredients;
+    if (!ingredients || !Array.isArray(ingredients) || ingredients.length === 0) {
+        res.status(400).send('Bad request');
+        return;
+    }
+    var id = Math.random().toString(36).substr(2, 9);
+    try {
+        cocktailDB.set(id, ingredients);
+        cocktailDB.save();
+        res.json({
+            id
+        })
+    } catch (e) {
+        res.status(500).send('Internal server error');
+        return;
+    }
 })
 
 app.get('/robots.txt', (req, res) => {
