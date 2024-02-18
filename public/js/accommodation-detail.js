7e5c1aceb7e6f5b885d43fa8a53a10efcc7743c6
yaniv <120222659+y4aniv@users.noreply.github.com>
2024-02-18 00:31:29 +0000

    scripts: add scroll trigger animation to commodities list

--- public/js/accommodation-detail.js
+++ public/js/accommodation-detail.js
@@ -7,3 +7,15 @@ setInterval(() => {
     document.getElementsByClassName('accommodation-detail__slider')[0].children[(sliderIndex + sliderLength - 1) % sliderLength].style.opacity = 0;
     console.log(sliderIndex);
 }, 3000);
+
+gsap.to('.commodities__list li', {
+    scrollTrigger: {
+        trigger: ".commodities__list",
+        start: "top 80%",
+        end: "bottom center",
+        scrub: 1
+    },
+    opacity: 1,
+    y: 0,
+    stagger: 0.2
+});
