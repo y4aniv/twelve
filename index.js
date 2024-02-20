bbad91d36ef7d35d6d2140f491f45ab8f1174bd7
yaniv <120222659+y4aniv@users.noreply.github.com>
2024-02-20 00:04:25 +0000

    server: remove unused '/video' route

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(express.static('public'));
 app.use('/css', express.static(`${__dirname}public/css`));
 app.use('/js', express.static(`${__dirname}public/js`));
 app.use('/img', express.static(`${__dirname}public/img`));
-app.use('/video', express.static(`${__dirname}public/video`));
 
 // Configurer EJS comme moteur de rendu
 app.set('view engine', 'ejs');
